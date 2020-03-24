7dc33f22d5a04921de02cfcf12852b1244140615
Charlie <cpmchugh1@gmail.com>
2020-03-24 16:59:22 +0000

    Merge sort - WIP

--- src/edu/princeton/algorithms/sorting/lessons/Sort.java
+++ src/edu/princeton/algorithms/sorting/lessons/Sort.java
@@ -7,9 +7,13 @@ public class Sort {
     private static final Map<String, Algorithm> algorithmLookup = Map.of(
             "bubble", Algorithm.Bubble,
             "selection", Algorithm.Selection,
-            "insertion", Algorithm.Insertion);
+            "insertion", Algorithm.Insertion,
+            "merge", Algorithm.Merge);
+
+    // Sorting methods
 
     public static <T extends Comparable<T>> T[] bubble(T[] inputArray) {
+        validateInputArray(inputArray);
         var array = inputArray.clone();
 
         for (var i = 0; i < array.length - 1; i++) {
@@ -20,10 +24,12 @@ public class Sort {
             }
         }
 
+        assert isSorted(array);
         return array;
     }
 
     public static <T extends Comparable<T>> T[] selection(T[] inputArray) {
+        validateInputArray(inputArray);
         var array = inputArray.clone();
 
         for (var i = 0; i < array.length; i++) {
@@ -37,10 +43,12 @@ public class Sort {
             swap(array, i, currentMin);
         }
 
+        assert isSorted(array);
         return array;
     }
 
     public static <T extends Comparable<T>> T[] insertion(T[] inputArray) {
+        validateInputArray(inputArray);
         var array = inputArray.clone();
 
         for (var i = 1; i < array.length; i++) {
@@ -53,8 +61,91 @@ public class Sort {
             }
         }
 
+        assert isSorted(array);
         return array;
     }
+
+    public static <T extends Comparable<T>> T[] merge(T[] inputArray) {
+        validateInputArray(inputArray);
+        var array = inputArray.clone();
+
+        // auxArray is for copying across when merging.
+        var auxArray = inputArray.clone();
+        mergeSort(array, auxArray, 0, array.length - 1);
+        return array;
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(T[] array, T[] auxArray, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+
+        var mid = (high + low) / 2;
+        mergeSort(array, auxArray, low, mid); // First half
+        mergeSort(array, auxArray, mid + 1, high); // Second half
+        merge(array, auxArray, low, mid, high);
+
+        assert isSorted(array);
+    }
+
+    private static <T extends Comparable<T>> void merge(
+            T[] array,
+            T[] auxArray,
+            int low,
+            int mid,
+            int high) {
+
+        for (var i = low; i <= high; i++) {
+            auxArray[i] = array[i];
+        }
+
+        var lowInd = low;
+        var highInd = mid + 1;
+
+        for (var i = low; i <= high; i++) {
+            if (lowInd > mid) { // Low has no more elements
+                array[i] = auxArray[highInd++];
+            } else if (highInd > high) { // High has no more elements
+                array[i] = auxArray[lowInd++];
+            } else if (auxArray[lowInd].compareTo(auxArray[highInd]) > 0) {
+                array[i] = auxArray[highInd++];
+            }
+        }
+    }
+
+    // Sorting helper methods
+
+    private static <T> void swap(T[] array, int index1, int index2) {
+        var temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
+    }
+
+    private static <T extends Comparable<T>> boolean isSorted(T[] array) {
+        return isSorted(array, 0, array.length - 1);
+    }
+
+    private static <T extends Comparable<T>> boolean isSorted(T[] array, int low, int high) {
+        for (var i = low; i < high - 1; i++) {
+            if (array[i].compareTo(array[i + 1]) > 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static <T extends Comparable<T>> void validateInputArray(T[] inputArray) {
+        if (inputArray == null) {
+            throw new IllegalArgumentException("Null array passed to method.");
+        }
+
+        if (inputArray.length == 0) {
+            throw new IllegalArgumentException("Empty array passed to method.");
+        }
+    }
+
+    // Entry point
 
     public static void main(String[] args) throws Exception {
         validateInput(args);
@@ -79,6 +170,9 @@ public class Sort {
             case Insertion:
                 sortedArray = Sort.insertion(randomArray);
                 break;
+            case Merge:
+                sortedArray = Sort.merge(randomArray);
+                break;
             default:
                 throw new Exception();
         }
@@ -89,11 +183,7 @@ public class Sort {
         System.in.read();
     }
 
-    private static <T> void swap(T[] array, int index1, int index2) {
-        var temp = array[index1];
-        array[index1] = array[index2];
-        array[index2] = temp;
-    }
+    // Main method helpers
 
     private static void validateInput(String[] args) {
         if (args == null || args.length == 0) {
@@ -146,6 +236,7 @@ public class Sort {
     private enum Algorithm {
         Bubble,
         Selection,
+        Merge,
         Insertion
     }
 }
